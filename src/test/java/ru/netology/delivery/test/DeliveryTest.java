518c4f7797bc9c60a754e9d751f09dc8167a3c36
AndresKorvin <andreskorvin@gmail.com>
2023-09-05 18:59:50 +0300

    3

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -31,84 +31,84 @@ class DeliveryTest {
         open("http://localhost:9999");
     }
 
-//    @ParameterizedTest
-//    @CsvSource({
-//            "Майкоп", "Горно-Алтайск", "Уфа", "Улан-Удэ", "Махачкала", "Донецк", "Магас", "Нальчик", "Элиста", "Черкесск",
-//            "Петрозаводск", "Сыктывкар", "Симферополь", "Луганск", "Йошкар-Ола", "Саранск", "Якутск", "Владикавказ", "Казань", "Кызыл",
-//            "Ижевск", "Абакан", "Грозный", "Чебоксары", "Барнаул", "Чита", "Петропавловск-Камчатский", "Краснодар", "Красноярск", "Пермь",
-//            "Владивосток", "Ставрополь", "Хабаровск", "Благовещенск", "Архангельск", "Астрахань", "Белгород", "Брянск", "Владимир", "Волгоград",
-//            "Вологда", "Воронеж", "Мелитополь", "Иваново", "Иркутск", "Калининград", "Калуга", "Кемерово", "Киров", "Кострома",
-//            "Курган", "Курск", "Гатчина", "Санкт-Петербург", "Липецк", "Магадан", "Красногорск", "Мурманск", "Нижний Новгород", "Великий Новгород",
-//            "Новосибирск", "Омск", "Оренбург", "Орёл", "Пенза", "Псков", "Ростов-на-Дону", "Рязань", "Самара", "Саратов",
-//            "Южно-Сахалинск", "Екатеринбург", "Смоленск", "Тамбов", "Тверь", "Томск", "Тула", "Тюмень", "Ульяновск", "Херсон",
-//            "Челябинск", "Ярославль", "Москва", "Санкт-Петербург", "Севастополь", "Биробиджан", "Нарьян-Мар", "Ханты-Мансийск", "Анадырь", "Салехард"
-//    })
-//    void allTownTesting(String administrativeCenter) {
-//
-//        $("[data-test-id='city'] .input__control").setValue(administrativeCenter);
-//        $("[data-test-id='date'] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-//        $("[data-test-id='date'] input").setValue(generateDate (3));
-//        $("[data-test-id='name'] .input__control").setValue("Имя");
-//        $("[data-test-id='phone'] .input__control").setValue("+79999999999");
-//        $("[data-test-id='agreement']").click();
-//        $("button span.button__text").click();
-//
-//        $("[data-test-id=city].input_invalid .input__sub").shouldNotBe(visible);
-//        $(".notification__content").shouldBe(visible, Duration.ofSeconds(2));
-//        $("[data-test-id='success-notification'] .notification__content").shouldHave(exactText("Встреча успешно запланирована на " + generateDate(3)));
-//
-//    }
-//
-//    @ParameterizedTest
-//    @CsvSource({
-//            "Игорь-Геннадьевич", "Фёкла Васильевна"
-//    })
-//    void nameTesting(String name) {
-//
-//        $("[data-test-id='city'] .input__control").setValue("Майкоп");
-//        $("[data-test-id='date'] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-//        $("[data-test-id='date'] input").setValue(generateDate(3));
-//        $("[data-test-id='name'] .input__control").setValue(name);
-//        $("[data-test-id='phone'] .input__control").setValue("+79999999999");
-//        $("[data-test-id='agreement']").click();
-//        $("button span.button__text").click();
-//
-//        $("[data-test-id=city].input_invalid .input__sub").shouldNotBe(visible);
-//        $(".notification__content").shouldBe(visible, Duration.ofSeconds(2));
-//        $("[data-test-id='success-notification'] .notification__content").shouldHave(exactText("Встреча успешно запланирована на " + generateDate(3)));
-//
-//    }
-//
-//    @ParameterizedTest
-//    @CsvSource({
-//            "Alex", // латиница
-//            "Турчинский~", // спецсимволы
-//            "Турчинский!", // спецсимволы
-//            "Турчинский№", // спецсимволы
-//            "Турчинский;", // спецсимволы
-//            "Турчинский%", // спецсимволы
-//            "Турчинский:", // спецсимволы
-//            "Турчинский?", // спецсимволы
-//            "Турчинский*", // спецсимволы
-//            "Турчинский+", // спецсимволы
-//            "Турчинский=", // спецсимволы
-//            "Турчинский/", // спецсимволы
-//            "Турчинский.", // спецсимволы
-//            "123," // цифры
-//    })
-//    void shouldNotTakeName(String name) {
-//
-//        $("[data-test-id='city'] .input__control").setValue("Москва");
-//        $("[data-test-id='date'] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-//        $("[data-test-id='date'] input").setValue(generateDate(3));
-//        $("[data-test-id='name'] .input__control").setValue(name);
-//        $("[data-test-id='phone'] .input__control").setValue("+79996665544");
-//        $("[data-test-id='agreement']").click();
-//        $("button span.button__text").click();
-//        $("[data-test-id=name].input_invalid .input__sub")
-//                .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-//
-//    }
+    @ParameterizedTest
+    @CsvSource({
+            "Майкоп", "Горно-Алтайск", "Уфа", "Улан-Удэ", "Махачкала", "Донецк", "Магас", "Нальчик", "Элиста", "Черкесск",
+            "Петрозаводск", "Сыктывкар", "Симферополь", "Луганск", "Йошкар-Ола", "Саранск", "Якутск", "Владикавказ", "Казань", "Кызыл",
+            "Ижевск", "Абакан", "Грозный", "Чебоксары", "Барнаул", "Чита", "Петропавловск-Камчатский", "Краснодар", "Красноярск", "Пермь",
+            "Владивосток", "Ставрополь", "Хабаровск", "Благовещенск", "Архангельск", "Астрахань", "Белгород", "Брянск", "Владимир", "Волгоград",
+            "Вологда", "Воронеж", "Мелитополь", "Иваново", "Иркутск", "Калининград", "Калуга", "Кемерово", "Киров", "Кострома",
+            "Курган", "Курск", "Гатчина", "Санкт-Петербург", "Липецк", "Магадан", "Красногорск", "Мурманск", "Нижний Новгород", "Великий Новгород",
+            "Новосибирск", "Омск", "Оренбург", "Орёл", "Пенза", "Псков", "Ростов-на-Дону", "Рязань", "Самара", "Саратов",
+            "Южно-Сахалинск", "Екатеринбург", "Смоленск", "Тамбов", "Тверь", "Томск", "Тула", "Тюмень", "Ульяновск", "Херсон",
+            "Челябинск", "Ярославль", "Москва", "Санкт-Петербург", "Севастополь", "Биробиджан", "Нарьян-Мар", "Ханты-Мансийск", "Анадырь", "Салехард"
+    })
+    void allTownTesting(String administrativeCenter) {
+
+        $("[data-test-id='city'] .input__control").setValue(administrativeCenter);
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(generateDate (3));
+        $("[data-test-id='name'] .input__control").setValue("Имя");
+        $("[data-test-id='phone'] .input__control").setValue("+79999999999");
+        $("[data-test-id='agreement']").click();
+        $("button span.button__text").click();
+
+        $("[data-test-id=city].input_invalid .input__sub").shouldNotBe(visible);
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(2));
+        $("[data-test-id='success-notification'] .notification__content").shouldHave(exactText("Встреча успешно запланирована на " + generateDate(3)));
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Игорь-Геннадьевич", "Фёкла Васильевна"
+    })
+    void nameTesting(String name) {
+
+        $("[data-test-id='city'] .input__control").setValue("Майкоп");
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(generateDate(3));
+        $("[data-test-id='name'] .input__control").setValue(name);
+        $("[data-test-id='phone'] .input__control").setValue("+79999999999");
+        $("[data-test-id='agreement']").click();
+        $("button span.button__text").click();
+
+        $("[data-test-id=city].input_invalid .input__sub").shouldNotBe(visible);
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(2));
+        $("[data-test-id='success-notification'] .notification__content").shouldHave(exactText("Встреча успешно запланирована на " + generateDate(3)));
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Alex", // латиница
+            "Турчинский~", // спецсимволы
+            "Турчинский!", // спецсимволы
+            "Турчинский№", // спецсимволы
+            "Турчинский;", // спецсимволы
+            "Турчинский%", // спецсимволы
+            "Турчинский:", // спецсимволы
+            "Турчинский?", // спецсимволы
+            "Турчинский*", // спецсимволы
+            "Турчинский+", // спецсимволы
+            "Турчинский=", // спецсимволы
+            "Турчинский/", // спецсимволы
+            "Турчинский.", // спецсимволы
+            "123," // цифры
+    })
+    void shouldNotTakeName(String name) {
+
+        $("[data-test-id='city'] .input__control").setValue("Москва");
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(generateDate(3));
+        $("[data-test-id='name'] .input__control").setValue(name);
+        $("[data-test-id='phone'] .input__control").setValue("+79996665544");
+        $("[data-test-id='agreement']").click();
+        $("button span.button__text").click();
+        $("[data-test-id=name].input_invalid .input__sub")
+                .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+
+    }
 
     @Test
     @DisplayName("Should successful plan and replan meeting")
